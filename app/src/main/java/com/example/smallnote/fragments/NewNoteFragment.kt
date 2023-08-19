4c5d7bea641e1ab078d88479516e1badb8637764
ziad-ezzat <ezzatziad83@gmail.com>
2023-08-19 21:49:23 +0200

    first commit

---
+++ app/src/main/java/com/example/smallnote/fragments/NewNoteFragment.kt
@@ -0,0 +1,8 @@
+package com.example.smallnote.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.smallnote.R
+
+class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
+
+}
---
+++ app/src/main/java/com/example/smallnote/fragments/NotesFragment.kt
@@ -0,0 +1,9 @@
+package com.example.smallnote.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.smallnote.R
+
+class NotesFragment : Fragment(R.layout.fragment_notes) {
+
+
+}
